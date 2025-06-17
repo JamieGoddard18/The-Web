4ce0c8283670cb13e76fffbf261e39c99d73f8cb
JamieGoddard18 <jamiegoddard24@gmail.com>
2025-06-17 09:15:07 +0100

    Comments

--- convert.py
+++ convert.py
@@ -1,9 +1,8 @@
+# Convert xlxs to JSON 
 import pandas as pd
 
-# Replace this with your Excel filename
 df = pd.read_excel("The Web.xlsx")
 
-# Output as a JSON file
 df.to_json("graph-data.json", orient="records", indent=2)
 
-print("Excel converted to JSON successfully.")
+print("Excel converted to JSON.")
